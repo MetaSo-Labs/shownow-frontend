afe126689a8bfabc4c177f7b2947d94d9b4c1c13
feiyang1020 <feiyangl1020@163.com>
2025-01-10 18:46:22 +0800

    chore:responseInterceptors 401

--- src/app.tsx
+++ src/app.tsx
@@ -1,4 +1,25 @@
+import type { RequestConfig } from 'umi';
+import { history } from 'umi';
+window.BUILD_ENV = (process.env.BUILD_ENV || 'testnetDev') as Window["BUILD_ENV"];
+export const request: RequestConfig = {
+    timeout: 10000,
+    validateStatus: function (status: number) {
+        return status >= 200 && status < 300; // default
+    },
+    requestInterceptors: [],
+    responseInterceptors: [
+        [
+            (response) => {
+                return response
+            }, (error: any) => {
+                console.log('error', error);
+                if (error && error.response && error.response.status === 401) {
+                    history.push('/dashboardLogin');
+                }
 
-window.BUILD_ENV = process.env.BUILD_ENV || '';
-console.log('BUILD_ENV:', window.BUILD_ENV);
+                return Promise.reject(error)
+            }
+        ]
+    ]
+};
 
